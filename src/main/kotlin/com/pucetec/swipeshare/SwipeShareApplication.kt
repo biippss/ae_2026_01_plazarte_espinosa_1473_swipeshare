b0dd6f598b6d422fafffa5c84c6bf7ad6e2c0536
biippss <brandonplazarte25@gmail.com>
2026-07-27 22:19:32 -0500

    commit inicial proyecto integrador

---
+++ src/main/kotlin/com/pucetec/swipeshare/SwipeShareApplication.kt
@@ -0,0 +1,11 @@
+package com.pucetec.swipeshare
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SwipeShareApplication
+
+fun main(args: Array<String>) {
+	runApplication<SwipeShareApplication>(*args)
+}
---
+++ src/test/kotlin/com/pucetec/swipeshare/SwipeShareApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.pucetec.swipeshare
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SwipeShareApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
