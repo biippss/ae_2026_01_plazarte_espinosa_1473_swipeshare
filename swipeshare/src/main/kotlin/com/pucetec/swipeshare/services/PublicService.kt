3807adfc33b7da5058ee0879da3089b2c6da951c
biippss <brandonplazarte25@gmail.com>
2026-08-12 09:55:46 -0500

    feat: standardize logging format and fix inter-service communication

--- swipeshare/src/main/kotlin/com/pucetec/swipeshare/services/PublicService.kt
+++ swipeshare/src/main/kotlin/com/pucetec/swipeshare/services/PublicService.kt
@@ -3,6 +3,7 @@ package com.pucetec.swipeshare.services
 import com.pucetec.swipeshare.dto.StatsResponse
 import com.pucetec.swipeshare.repositories.ItemRepository
 import com.pucetec.swipeshare.repositories.MatchRepository
+import org.slf4j.LoggerFactory
 import org.springframework.stereotype.Service
 
 @Service
@@ -10,9 +11,13 @@ class PublicService(
     private val itemRepository: ItemRepository,
     private val matchRepository: MatchRepository
 ) {
+    private val logger = LoggerFactory.getLogger(PublicService::class.java)
+
+    // Obtiene las estadísticas públicas globales del sistema
     fun getGlobalStats(): StatsResponse {
         val totalItems = itemRepository.count()
         val totalMatches = matchRepository.count()
+        logger.info("event=public.stats_viewed | msg=Global statistics calculated")
         return StatsResponse(
             totalItems = totalItems,
             totalMatches = totalMatches
